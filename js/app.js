8497df1dd9a25514afaccef2fc44e68e62538c25
AzamatOrtiqovdev <ortiqovazamat890@gmail.com>
2023-05-09 10:23:51 +0500

    Update all code

--- js/app.js
+++ js/app.js
@@ -1,11 +1,43 @@
 const locationElements = document.querySelector("#location"),
       cardElement = document.querySelector("#card"),
       detailsElement = document.querySelector("#details"),
-      wetherIconElement = document.querySelector("#wether-icon")
+      wetherIconElement = document.querySelector("#wether-icon"),
+      overlayElements = document.querySelector('#overlay')
+
+locationElements.city.focus()
+
+// loader
+function loader(status) {
+      if(status) {
+            overlayElements.classList.remove("d-none")
+      } else {
+            overlayElements.classList.add("d-none")
+      }
+}
+
+// Upadate UI
+const updateUI = (wether) => {
+      // console.log(wether)
+      detailsElement.innerHTML = `
+      <h5 class="mb-3">${wether.name}, ${wether.sys.country}</h5>
+      <p class="mb-3">${wether.weather[0].main}, ${wether.weather[0].description}</p>
+      <div class="display-4 mb-3">
+            <span>${Math.round(wether.main.temp)}</span>
+            <span>&deg;C</span>
+      </div>
+      `
+      if(cardElement.classList.contains("d-none")) {
+            cardElement.classList.remove("d-none")
+      }
+
+      wetherIconElement.src = `https://openweathermap.org/img/wn/${wether.weather[0].icon}@2x.png`
+
+}
+
 
 // get wether 
 const getWether = async (city) => {
-      const data = await getData(city)
+      const data = await getData(city) // This function runs in app.js, because We have attached js files to html file. For the getData function to work, first We must attach script.js file to index.html file, after We must attach app.js file to index.html file.
       return data
 }
 
@@ -16,4 +48,5 @@ locationElements.addEventListener("submit", (event) => {
       const cityName = locationElements.city.value.trim()
       locationElements.reset()
       getWether(cityName)
+            .then((data) => updateUI(data))
 })
--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@ const KEY = '85eb218234d1f9599736f1276536beeb'
 const getData = async (city) => {
 
       const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${KEY}`
+      loader(true)
       const request = await fetch(URL)
       const data = await request.json()
+      loader(false)
 
       return data
 }
